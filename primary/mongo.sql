28d20514c1b0d1c5577a1295400345bd78fb4bff
Aaron-23 <851882108@qq.com>
2019-12-27 09:31:55 +0800

    Update mongo.sql

--- primary/mongo.sql
+++ primary/mongo.sql
@@ -1,10 +1,10 @@
 rs.initiate();
 cfg = {
-  _id: 'rs0',
+  _id: 'rainbond',
   members: [
-    { _id: 0, host: '127.0.0.1:27017' },
-    { _id: 1, host: '127.0.0.1:27018' },
-    { _id: 2, host: '127.0.0.1:27019' }
+    { _id: 3, host: '127.0.0.1:27017' },
+    { _id: 2, host: '127.0.0.1:27018' },
+    { _id: 1, host: '127.0.0.1:27019' }
   ]
 };
 cfg.protocolVersion = 1;
