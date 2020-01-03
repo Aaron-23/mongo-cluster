2db7d1fbdad463f84bc515606ddc7a937c5df9ef
Aaron-23 <851882108@qq.com>
2020-01-03 17:24:25 +0800

    Update docker-entrypoint.sh

--- primary/docker-entrypoint.sh
+++ primary/docker-entrypoint.sh
@@ -29,11 +29,11 @@ sleep 10
 CURRENT_POD_NUM=$(nslookup ${SERVICE_NAME} | grep Address | sed '1d' | awk '{print $2}' | wc -l)
 [[ $DEBUG ]] && echo $(nslookup ${SERVICE_NAME})> ./logfile
 if [[ $CURRENT_POD_NUM -gt 1 ]];then
-    sed -i '$a\discovery.zen.ping.unicast.hosts' /usr/share/elasticsearch/config/elasticsearch.yml
+    sed -i '$a\discovery.zen.ping.unicast.hosts' /mongo.sql
     ip=$(nslookup ${SERVICE_NAME} | grep Address | sed '1d' | awk '{print $2}')
     ips=$(echo $ip | tr ' ' ',')
     [[ $DEBUG ]] && echo ${ip} >> ./logfile
-    sed -i "s/discovery.zen.ping.unicast.hosts*/discovery.zen.ping.unicast.hosts: [${ips}]/g" /usr/share/elasticsearch/config/elasticsearch.yml
+    sed -i "s/discovery.zen.ping.unicast.hosts*/discovery.zen.ping.unicast.hosts: [${ips}]/g" /mongo.sql
 fi
     
 [[ $PAUSE ]] && sleep $PAUSE
